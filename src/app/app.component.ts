eb833c7b1475d5cb17438812af07060f7a9447b9
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2024-04-04 15:54:43 -0300

    start angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './models/employee';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    { id: 1, name: 'John', country: 'United States' },
+    { id: 2, name: 'Matias', country: 'United States' },
+    { id: 3, name: 'Mohn', country: 'United States' },
+  ];
 }
